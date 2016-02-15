d3e5f5509b062ceb4268a9669ce204a81dfa97c7
DavidYi1 <yid@bxscience.edu>
2016-02-15 17:23:51 -0500

    Update Borrower.java

--- Borrower.java
+++ Borrower.java
@@ -1,15 +1,5 @@
 public abstract class Borrower {
   
-  private Book[] books;
-  private int checkout;
-  
-  public Borrower(Book[] b, int c) {
-    books = new Book[b.length];
-    for (int i = 0; i < b.length; i++)
-      books[i] = b[i];
-    checkout = c;
-  }
-  
   public abstract void borrow();
   public abstract void returnBook();
 }
