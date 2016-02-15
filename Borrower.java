f5f8d8e6cc0efdb7b3ec9b981f57d9f787e26c6e
Chelsea Wang <wangc9@bxscience.edu>
2016-02-15 16:57:35 -0500

    Update Borrower.java

--- Borrower.java
+++ Borrower.java
@@ -1,7 +1,17 @@
+import java.util.ArrayList;
 public abstract class Borrower {
   
-  public abstract void borrow();
+  private ArrayList<Book> books;
+  private int checkout;
   
+  public class Borrower(ArrayList<Book> b, int c) {
+    books = new ArrayList<Book>();
+    for (int i = 0; i < b.size(); i++)
+      books.set(i, b.get(i));
+    checkout = c;
+  }
+  
+  public abstract void borrow();
   public abstract void returnBook();
   
 }
