06470e9d8ce7210b160d0e31d81c7f209f5e324f
Chelsea Wang <wangc9@bxscience.edu>
2016-02-21 00:31:26 -0500

    Update Borrower.java

--- Borrower.java
+++ Borrower.java
@@ -3,6 +3,18 @@ public abstract class Borrower {
   private Book[] books;
   private int daysDue;
   
-  public abstract void borrow();
+  public Borrower() {
+    books = new Book[0];
+  }
+  
+  public void borrow(Book b) {
+    for (int i = 0; i < books.length; i++) {
+      if (books[i] == null) {
+        books[i] = b;
+        break;
+      }
+    }
+  }
+  
   public abstract void returnBook();
 }
