44b6fab6374e05d514f42a6053dd592de6fac7cf
DavidYi1 <yid@bxscience.edu>
2016-02-21 19:44:02 -0500

    This should work

--- Library-Project-master/Teacher.java
+++ Library-Project-master/Teacher.java
@@ -1,10 +1,7 @@
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Calendar;
 public class Teacher extends Borrower {
   
   private Book[] books = new Book[4]; //need this here for borrow method to work
-  private Date[] dueDate = new Date[4];
+  private String[] dueDates = new String[4];
   private int daysDue = 7, id;
   private String name;
   
@@ -13,30 +10,16 @@ public class Teacher extends Borrower {
     name = n;
   }
   
-  public boolean noSpace(){
-    for (int i = 0; i < books.length; i++) {
-        if (books[i] == null) {
-          return false;
-        }
-      }
-    return true;
-  }
   public void borrow(Book b) {
-    super.borrow(b);
-  }
-  
-  public void fullSpace(){
-    System.out.print("You can not borrow anymore books");
-  }
-  
-  public void returnBook(Book b) {
     for (int i = 0; i < books.length; i++) {
-        if (books[i] == b) {
-          books[i] = null;
-          dueDate[i] = null;
-          return;
-        }
+      if (books[i] == null) {
+        books[i] = b;
+        break;
       }
-    System.out.print("No such book borrowed");
+    }
+  }
+  
+  public void returnBook() {
+    
   }
 }
