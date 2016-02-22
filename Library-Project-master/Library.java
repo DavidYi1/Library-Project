e0fd051dc0a1daec3621df144e2d3654446c8ede
DavidYi1 <yid@bxscience.edu>
2016-02-21 19:45:43 -0500

    Added files via upload

--- Library-Project-master/Library.java
+++ Library-Project-master/Library.java
@@ -1,1 +1,19 @@
 import java.util.ArrayList;
+
+public class Library{
+  
+  public static void main (String args[]){
+    private boolean run = true;
+    String command = null;
+    BufferedReader br = new BufferedReader(new FileReader("books.txt"));
+    PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("books.txt")));
+    
+    
+    System.out.print("What would you like to access? (borrower/librarian)");
+    
+    
+    
+  }
+  
+  
+}
