ec161a4cc6606649280641b7ed5992402913ce56
DavidYi1 <yid@bxscience.edu>
2016-02-21 19:45:56 -0500

    Added files via upload

--- Library-Project-master/Student.java
+++ Library-Project-master/Student.java
@@ -1,7 +1,11 @@
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Calendar;
+
 public class Student extends Borrower {
   
   private Book[] books = new Book[2]; // I want to make sure that they exist as permanent fields instead of object fields
-  private String[] dueDates = new String[2];
+  private Date[] dueDate = new Date[2];
   private int daysDue = 14, osis, grade;
   private String lastName, firstName, officialClass;
   
@@ -12,17 +16,31 @@ public class Student extends Borrower {
     firstName = fn;
     officialClass = oc;
   }
-  
-  public void borrow(Book b) {
+  public boolean noSpace(){
     for (int i = 0; i < books.length; i++) {
-      if (books[i] == null) {
-        books[i] = b;
-        break;
+        if (books[i] == null) {
+          return false;
+        }
       }
-    }
+    return true;
+  }
+  public void borrow(Book b) {
+    super.borrow(b);
   }
   
-  public void returnBook() {
-    
+  public void fullSpace(){
+    System.out.print("You can not borrow anymore books");
   }
+  
+  public void returnBook(Book b) {
+    for (int i = 0; i < books.length; i++) {
+        if (books[i] == b) {
+          books[i] = null;
+          dueDate[i] = null;
+          return;
+        }
+      }
+    System.out.print("No such book borrowed");
+  }
+  
 }
