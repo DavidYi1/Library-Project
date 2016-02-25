6faa3d5a63921bd4a7b4dac3d02111515ed156d1
DavidYi1 <yid@bxscience.edu>
2016-02-25 00:59:34 -0500

    Added files via upload

--- Library-Project-master/Book.java
+++ Library-Project-master/Book.java
@@ -1,6 +1,8 @@
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Calendar;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class Book {
   //Fields
@@ -9,32 +11,37 @@ public class Book {
   private String genre;
   private String author;
   private String status;
-  private boolean checkedOut;
-  private int daysDue;
   private Date dueDate;
+  private boolean checkedOut;
+
   
-  public Book(String t, String i, String g, String a, String s) {
-    //need to fix to account for if book is checked out or not/
+  public Book(String t, String i, String g, String a, String s, String dd, String check) {
     title = t;
     isbn = i;
     genre = g;
     author = a;
     status = s;
-    checkedOut = false;
-    daysDue = 0;
-    dueDate = null;
+    //if there exists nothing between s and check then this should be false and the book is in the library
+    if (!dd.equals("")){
+      dueDate = convertStringToDate(dd);
+    }
+    else 
+      dueDate = null;
+    
+    //checks to see if checkedOut or not. Should be consistent with dueDate so useful as a debugging tool.
+    if (check.equals("true"))
+      checkedOut = true;
+    else 
+      checkedOut = false;
+    
   }
   
-  public String getTitle() {
+  public String getTitle(){
     return title;
   }
   
-  public String getISBN() {
+  public String getISBN(){
     return isbn;
-  }
-  
-  public void setCheckoutTime(int dd) {
-    daysDue = dd;
   }
   
   public void bookCheckedOut(Date dueIn) {
@@ -46,5 +53,24 @@ public class Book {
     checkedOut = false;
     dueDate = null;
   }
+  public Date convertStringToDate(String dateString)
+  {
+    Date date = null;
+    Date formatteddate = null;
+    DateFormat df = new SimpleDateFormat("MMM dd, yyyy");
+    try{
+        date = df.parse(dateString);
+
+    }
+    catch ( Exception ex ){
+        System.out.println(ex);
+    }
+    return date;
+  }
   
+  public String convertDateToString(Date dd){
+   SimpleDateFormat sdfr = new SimpleDateFormat("MMM dd, yyyy");
+   String dateString = sdfr.format(dd);
+   return dateString;
+  }
 }
